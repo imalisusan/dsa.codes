eaa48dd39f6761d6b4501e0ef528d85f0b6ffa22
Imali Susan <35591292+imalisusan@users.noreply.github.com>
2022-10-25 10:38:05 +0300

    Update FizzBuzz.php

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -1,1 +1,49 @@
-//
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return String[]
+     */
+
+  
+    function fizzBuzz($n) {
+        $numbers = array();
+    for($j=1; $j<=$n; $j++ )
+    {    
+    if($j%3==0)
+    {
+        $divisibleByThree= true;
+    }
+    else
+    {
+        $divisibleByThree= false;   
+    }
+    if($j%5 == 0)
+    {
+        $divisibleByFive= true;
+    }
+     else
+    {
+        $divisibleByFive= false;   
+    }    
+        if($divisibleByThree == true && $divisibleByFive == true)
+        {
+             $numbers[$j] = "FizzBuzz";
+        }
+        else if($divisibleByThree == true)
+        {
+            $numbers[$j] = "Fizz";
+        }
+        else if($divisibleByFive == true)
+        {
+             $numbers[$j] = "Buzz";
+        }
+        else
+        {
+            $numbers[$j] = (string)$j;
+        }
+        
+    }   
+    return $numbers;
+    }
+}

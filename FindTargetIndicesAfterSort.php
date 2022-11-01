b077ecd215842be72a09ecea28aed73a0e1d2804
Imali Susan <35591292+imalisusan@users.noreply.github.com>
2022-11-01 17:45:12 +0300

    Update FindTargetIndicesAfterSort.php

--- FindTargetIndicesAfterSort.php
+++ FindTargetIndicesAfterSort.php
@@ -9,20 +9,12 @@ class Solution {
          //code here
    $n = count($nums) - 1;
    $j= 1;
-   for ($i= 0; $i < $n; $i++) {
-        for ($j = $i; $j < $n; $j++) {
-            $jj = $j+1;
-            // Swap adjacent elements if they are in decreasing order
-            if ($nums[$i] > $nums[$jj]) {
-                list($nums[$j],$nums[$jj]) = array($nums[$jj],$nums[$j]);
-            }
-        } 
-    }
- 
+   sort($nums);
+
   $indices = array();
   for($l=0; $l <= $n; $l++)
   {
-    if($target == $nums[$l])
+    if($target === $nums[$l])
     {
     array_push($indices, $l);
     }
